95f95926b2e8194f5246bcfb7242aaf67b331ed8
koutaro0205 <koutaro130205@icloud.com>
2022-03-24 15:25:58 +0900

    Implement basic login

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.1.5/lib/assets/compiled/rails-ujs.js"
