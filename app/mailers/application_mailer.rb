ffc86e2edde751e2f9e94cda56049bc2d1a92776
koutaro0205 <koutaro130205@icloud.com>
2022-04-28 11:21:29 +0900

    edit application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@example.com",
-  # charset: 'ISO-2022-JP'
+  default from: "noreply@example.com"
+  # , charset: 'ISO-2022-JP'
   layout "mailer"
 end
