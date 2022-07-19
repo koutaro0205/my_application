3d921a59e13fe730a3acdfcd9ae398feac66f4b9
koutaro0205 <koutaro130205@icloud.com>
2022-07-19 16:17:21 +0900

    setting config/environments/~

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -73,8 +73,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'gmail.com',
-  user_name:            'inouekoutaro130205@gmail.com',
-  password:             'pyfjzbjlwmehcjke',
+  user_name:            '',
+  password:             '',
   authentication:       'plain',
   enable_starttls_auto:  true
 }
